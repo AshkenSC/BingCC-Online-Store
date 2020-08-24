d696eacc7640537a5b4ba205a11211e5c17717a9
ashken <393940378@qq.com>
2020-08-24 11:06:08 +0800

    Bug fix: Registration finish redirection fixed

--- BingCC/Controllers/AccountController.cs
+++ BingCC/Controllers/AccountController.cs
@@ -185,13 +185,19 @@ namespace BingCC.Controllers
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    //return RedirectToAction("Index", "Home");
+                    return RedirectToAction("DisplayEmail", "Account");
                 }
                 AddErrors(result);
             }
 
             // If we got this far, something failed, redisplay form
             return View(model);
+        }
+
+        public ActionResult DisplayEmail()
+        {
+            return View();
         }
 
         //
--- BingCC/Models/AccountViewModels.cs
+++ BingCC/Models/AccountViewModels.cs
@@ -74,30 +74,35 @@ namespace BingCC.Models
     public class RegisterViewModel
     {
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Incorrect email format.")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Display(Name = "User Name"), Required]
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username cannot be empty.")]
         public string UserName { get; set; }
 
-        [Display(Name = "First Name"), Required]
+        [Display(Name = "First name")]
+        [Required(ErrorMessage = "First name cannot be empty.")]
         public string FirstName { get; set; }
 
-        [Display(Name = "Last Name"), Required]
+        [Display(Name = "Last name")]
+        [Required(ErrorMessage = "Last name cannot be empty.")]
         public string LastName { get; set; }
 
-        [Display(Name = "Address"), Required]
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Address cannot be empty.")]
         public string Address { get; set; }
 
-        [Display(Name = "PhoneNumber"), Required]
-        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
+        [Required(ErrorMessage = "Phone number cannot be empty.")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Incorrect phone number format.")]
         public string PhoneNumber { get; set; }
 
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
+        [DataType(DataType.Password, ErrorMessage = "Your password is too young, too simple, sometimes naive.")]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
@@ -112,17 +117,17 @@ namespace BingCC.Models
     public class ResetPasswordViewModel
     {
         [Required]
-        [EmailAddress]
+        [EmailAddress (ErrorMessage = "Incorrect email format")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
+        //[DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
+        //[DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
